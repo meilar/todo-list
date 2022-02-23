8b3543c50e6a8f8e2fe28db50f3fd27356c1b0a5
Matt Eilar <meilar@gmail.com>
2022-02-23 10:35:57 -0800

    Revert "add description property to constructor"
    This reverts commit e785fe193ef657a5c390f4ec9c86a5a5f56da6f7.

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -9,22 +9,8 @@ namespace ToDoList.Tests
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      Item newItem = new Item("test");
+      Item newItem = new Item();
       Assert.AreEqual(typeof(Item), newItem.GetType());
-    }
-
-    [TestMethod]
-    public void GetDecription_ReturnsDescription_String()
-    {
-      //Arrange
-      string description = "Walk the dog.";
-      Item newItem = new Item(description);
-
-      //Act
-      string result = newItem.Description;
-
-      //Assert
-      Assert.AreEqual(description,result);
     }
   }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -2,11 +2,7 @@ namespace ToDoList.Models
 {
   public class Item
   {
-    public string Description { get; }
 
-    public Item(string description)
-    {
-      Description = description;
-    }
+
   }
 }
