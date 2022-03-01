3fb844d97bb14eaa702f85bde8673f157a307cc0
Matt Eilar <meilar@gmail.com>
2022-03-01 10:12:55 -0800

    feat: add support for multiple list items

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,28 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("/")]
+    [HttpGet("/")]
     public ActionResult Index()
     {
-      Item starterItem = new Item("Add first item to To Do List");
-      return View(starterItem);
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
     }
 
+    [HttpPost("/items")]
+    public ActionResult Create(string description)
+    {
+      Item myItem = new Item(description);
+      return RedirectToAction("Index");
+    }
     [Route("/items/new")]
     public ActionResult CreateForm()
     {
       return View();
-    }
-
-    [Route("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return View("Index", myItem);
     }
   }
 }
