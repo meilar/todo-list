2193f30e369bfa1cf19b9e73867f1236568da78e
Matt Eilar <meilar@gmail.com>
2022-03-01 09:49:04 -0800

    refactor project into MVC pattern webapplication

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,5 +1,20 @@
-// using System
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-// static void Main() {
-  
-// }
+namespace ToDoList
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
+    }
+  }
+}
